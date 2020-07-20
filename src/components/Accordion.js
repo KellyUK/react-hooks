d0cb7a39b1483e4afda7005a9e8b696fad22c2aa
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-07-20 18:29:25 +0100

    implement use state for accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,29 +5,28 @@ const Accordion = ({ items }) => {
 
   const renderedItems = items.map((items, index) => {
     const { content, title } = items;
+    const active = index === activeIndex ? 'active' : '';
 
     const onTitleClicked = index => {
       setActiveIndex(index);
     };
     return (
       <React.Fragment key={title}>
-        <div className='title active' onClick={() => onTitleClicked(index)}>
+        <div
+          className={`title ${active}`}
+          onClick={() => onTitleClicked(index)}
+        >
           <i className='dropdown icon'></i>
           {title}
         </div>
-        <div className='content active'>
+        <div className={`content ${active}`}>
           <p>{content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return (
-    <div className='ui styled accordion'>
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className='ui styled accordion'>{renderedItems}</div>;
 };
 
 export default Accordion;
