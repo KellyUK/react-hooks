cc08f6126acb898b799e0fc4aaa38b23841c9fb0
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-07-23 16:54:12 +0100

    add clean up function for dropdown component

--- src/App.js
+++ src/App.js
@@ -35,17 +35,27 @@ const options = [
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
       <h1>Hooks App</h1>
+      <button
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        Toggle Dropdown
+      </button>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      />
+      {showDropdown ? (
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      ) : null}
     </div>
   );
 };
