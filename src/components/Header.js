f9216c1abcf59112de96430a9d271532563521e8
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-07-24 13:17:00 +0100

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className='ui secondary pointing menu'>
-      <div className='item'>
-        <Link to='/search'>Search</Link>
-      </div>
-      <div className='item'>
-        <Link to='/dropdown'>Dropdown</Link>
-      </div>
-      <div className='item'>
-        <Link to='/translate'>Translate</Link>
-      </div>
-      <div className='item'>
-        <Link to='/accordion'>Accordion</Link>
+    <div>
+      <h1>Learning React Hooks App</h1>
+      <div className='ui secondary pointing menu'>
+        <div className='item'>
+          <Link to='/search'>Search</Link>
+        </div>
+        <div className='item'>
+          <Link to='/dropdown'>Dropdown</Link>
+        </div>
+        <div className='item'>
+          <Link to='/translate'>Translate</Link>
+        </div>
+        <div className='item'>
+          <Link to='/accordion'>Accordion</Link>
+        </div>
       </div>
     </div>
   );
